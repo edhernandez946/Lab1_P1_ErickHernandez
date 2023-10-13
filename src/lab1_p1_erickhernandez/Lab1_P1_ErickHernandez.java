a5f75e66ae6ef87543d368027e346b22a8825a7d
Daniel Hernandez <edhernandez946@gmail.com>
2023-10-13 14:13:17 -0700

    Uploaded File

--- src/lab1_p1_erickhernandez/Lab1_P1_ErickHernandez.java
+++ src/lab1_p1_erickhernandez/Lab1_P1_ErickHernandez.java
@@ -1,20 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1_p1_erickhernandez;
-
-/**
- *
- * @author Erick
- */
+import java.util.Scanner;
 public class Lab1_P1_ErickHernandez {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        
+        System.out.println("Elija una opcion");
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+
+        
+        if (numero == 1) {
+            
+            int num1 = 15;
+            int num2 = 30;
+            int resultado;
+            resultado = num1 * num2;
+            System.out.println("Resultado: " + resultado);
+            
+        } else if (numero == 2) {
+            
+            System.out.println("Ingrese 3 numeros");
+            int numeroUsuario1 = scanner.nextInt();
+            int numeroUsuario2 = scanner.nextInt();
+            int numeroUsuario3 = scanner.nextInt();
+            
+            double promedio = (numeroUsuario1 + numeroUsuario2 + numeroUsuario3) / (3);
+            System.out.println("El promedio de los 3 numeros es " + promedio);
+            
+        } else {
+            System.out.println("Opcion invalida");
+        }
+    }  
 }
